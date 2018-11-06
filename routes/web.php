ce8377265ae24b503ff1153434cf74e6107f6667
yama159 <fehiya@gmail.com>
2018-11-06 15:53:34 -0200

    06112018 - merge version

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/redirect/{social}','Auth\LoginController@socialLogin')->where('social','twitter|facebook|linkedin|google');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('{slug}', function() {
+    return view('home');
+})->where('slug', '(?!api)([A-z\d-\/_.]+)?');
