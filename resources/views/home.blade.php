a9604e3fe72140d9cdf15fea5522472dfc8ee4d6
BreFranca <bre.sfranca@gmail.com>
2018-11-06 17:15:05 -0200

    FiniUpdate laravel to 5.7

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,16 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="{{asset('css/all.css')}}">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <title>RP Learning</title>
+    </head>
+    <body>
+        <div id='app'></div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
