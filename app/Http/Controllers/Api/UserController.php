97cd163bb46d7b2babad24b11f2010381669c8e0
yama159 <fehiya@gmail.com>
2018-11-09 11:11:10 -0200

    09112018 - users api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -45,4 +45,96 @@ class UserController extends Controller
         $user = JWTAuth::toUser($request->token);
         return response()->json(['result' => $user]);
     }
+
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try{
+            $users = User::All();
+        }catch(ModelNotFoundException $e){
+            return response()->json(400);
+            //400: Bad request. The standard option for requests that fail to pass validation.
+        }
+        return response()->json($users,200);
+        //200: OK. The standard success code and default option.
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try{
+            $user = User::findOrFail($id);
+        }catch(ModelNotFoundException $e){
+            return response()->json(400);
+            //400: Bad request. The standard option for requests that fail to pass validation.
+        }
+        return response()->json($user,200);
+        //200: OK. The standard success code and default option.
+    }
+
+    // /**
+    //  * Store a newly created resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function store(Request $request)
+    // {
+    //     try{
+    //         $user = User::create($request->all());
+    //     }catch(ModelNotFoundException $e){
+    //         return response()->json(400);
+    //         //400: Bad request. The standard option for requests that fail to pass validation.
+    //     }
+    //     return response()->json(201);
+    //     //201: Object created. Useful for the store actions.
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try{
+            $user = User::findOrFail($id);
+            $user->update($request->all());
+        }catch(ModelNotFoundException $e){
+            return response()->json(400);
+            //400: Bad request. The standard option for requests that fail to pass validation.
+        }
+        return response()->json(204);
+        //204: No content. When an action was executed successfully, but there is no content to return.
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try{
+            $user = User::findOrFail($id);
+            $user->delete();
+        }catch(ModelNotFoundException $e){
+            return response()->json(400);
+            //400: Bad request. The standard option for requests that fail to pass validation.
+        }
+        return response()->json(204);
+        //204: No content. When an action was executed successfully, but there is no content to return.
+    }
 }
