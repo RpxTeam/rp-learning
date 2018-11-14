fced9ca85fc506d3665cf92f0570670c67832eb5
yama159 <fehiya@gmail.com>
2018-11-14 17:28:36 -0200

    14112018 - merge dev

--- public/js/app.js
+++ public/js/app.js
@@ -17957,7 +17957,7 @@ function isStyledComponent(target) {
 // 
 
 
-var SC_ATTR = typeof process !== 'undefined' && Object({"NODE_ENV":"development"}).SC_ATTR || 'data-styled';
+var SC_ATTR = typeof process !== 'undefined' && Object({"MIX_PUSHER_APP_CLUSTER":"mt1","MIX_PUSHER_APP_KEY":"","NODE_ENV":"development"}).SC_ATTR || 'data-styled';
 
 var SC_VERSION_ATTR = 'data-styled-version';
 
