ce3fdb6bbe7a2704cdc3a71f8e8c8b3ad292a559
yama159 <fehiya@gmail.com>
2018-11-21 11:56:01 -0200

    21112018 - merge dev

--- routes/api.php
+++ routes/api.php
@@ -39,12 +39,16 @@ Route::resource('courses', 'Api\CoursesController');
 //lesson routes
 Route::resource('courses/{id}/lessons', 'Api\LessonController');
 
-//DataCourse routes
-Route::resource('users/{user}/courses', 'Api\DataCourseController');
-Route::post('users/{user}/courses/{course}', 'Api\DataCourseController@store');
-Route::put('users/{user}/courses/{course}', 'Api\DataCourseController@update');
+Route::group(['prefix' => 'auth'], function () {
+    
+    //DataCourse routes
+    Route::resource('users/{user}/courses', 'Api\DataCourseController');
+    Route::post('users/{user}/courses/{course}', 'Api\DataCourseController@store');
+    Route::put('users/{user}/courses/{course}', 'Api\DataCourseController@update');
+    
+    //DataLesson routes
+    Route::resource('users/{user}/courses/{course}/lessons', 'Api\DataLessonController');
+    Route::post('users/{user}/courses/{course}/lessons/{lesson}', 'Api\DataLessonController@store');
+    Route::put('users/{user}/courses/{course}/lessons/{lesson}', 'Api\DataLessonController@update');
 
-//DataLesson routes
-Route::resource('users/{user}/courses/{course}/lessons', 'Api\DataLessonController');
-Route::post('users/{user}/courses/{course}/lessons/{lesson}', 'Api\DataLessonController@store');
-Route::put('users/{user}/courses/{course}/lessons/{lesson}', 'Api\DataLessonController@update');
+});
