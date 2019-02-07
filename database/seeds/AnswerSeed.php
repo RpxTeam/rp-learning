dc70a83b2fcaec48aabac4fd722851dae73b2951
yama159 <fehiya@gmail.com>
2019-02-07 17:47:53 -0200

    20190207 - fix quiz seeds

--- database/seeds/AnswerSeed.php
+++ database/seeds/AnswerSeed.php
@@ -14,14 +14,14 @@ class AnswerSeed extends Seeder
         $faker = Faker\Factory::create();
 
         $items = [
-            ['id' => 1, 'text' => 'sprites', 'correct'=> 1],
+            ['id' => 1, 'text' => 'sprites' , 'correct'=> 1],
             ['id' => 2, 'text' => 'simply textured polygonal objects', 'correct'=> 0],
             ['id' => 3, 'text' => 'full polygon', 'correct'=> 0],
             ['id' => 4, 'text' => 'fully texture-mapped', 'correct'=> 0],
             ['id' => 5, 'text' => '3D Polygon', 'correct'=> 0],
         ];
-
-        App\Answer::create($items);
+        foreach($items as $item)
+        App\Answer::create($item);
 
         // for($i=0;$i<15;$i++){
         //     App\Answer::create([
