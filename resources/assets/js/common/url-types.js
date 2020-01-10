74cc87aa05cab4f826629255e6df3dfded1bf1ee
yama159 <fehiya@gmail.com>
2020-01-10 10:00:29 -0300

    minor fixes

--- resources/assets/js/common/url-types.js
+++ resources/assets/js/common/url-types.js
@@ -1,4 +1,5 @@
+export const API_URL = 'http://localhost:8000';
+// export const API_URL = 'http://rplearning-banco-carrefour.tecnologia.ws';
 // export const API_URL = 'http://rplearning.rperformancegroup.com';
 // export const API_URL = 'http://rperformancegroupdev.com';
-export const API_URL = 'http://localhost:8000';
 // export const API_URL = 'http://rplearning-rperformance.tecnologia.ws';
